c07e0ebdf0eba0456bbf25575218e0dd2ad363bf
MSI\dev <damh17@gmail.com>
2018-05-01 15:13:13 +0300

    Change DesiredCapabilities to MutableCapabilities (as new FirefoxOptions() etc...)

--- addressbook-web-tests/src/test/java/ru/nd/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/nd/addressbook/appmanager/ApplicationManager.java
@@ -1,12 +1,12 @@
 package ru.nd.addressbook.appmanager;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -58,22 +58,27 @@ public class ApplicationManager {
             }
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
+            MutableCapabilities options = new MutableCapabilities();
 
             if (browser.equals(BrowserType.FIREFOX)) {
                 //System.setProperty("webdriver.gecko.driver", "C:\\utils\\geckodriver\\geckodriver.exe");
-                capabilities = DesiredCapabilities.firefox();
+                //capabilities = DesiredCapabilities.firefox();
+                options = new FirefoxOptions();
+                //capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win8.1")));
             } else if (browser.equals(BrowserType.CHROME)) {
                 //System.setProperty("webdriver.chrome.driver", "C:\\utils\\chromedriver\\chromedriver.exe");
-                capabilities = DesiredCapabilities.chrome();
+                //capabilities = DesiredCapabilities.chrome();
+                options = new ChromeOptions();
             } else if (browser.equals(BrowserType.IE)) {
                 //System.setProperty("webdriver.ie.driver", "C:\\utils\\iedriver\\IEDriverServer.exe");
-                capabilities = DesiredCapabilities.internetExplorer();
+                //capabilities = DesiredCapabilities.internetExplorer();
+                options = new InternetExplorerOptions();
             }
 
             //capabilities.setBrowserName(browser);
             //capabilities.setPlatform(org.openqa.selenium.Platform.WINDOWS);
             capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win8.1")));
-            driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+            driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), options);//capabilities);
         }
 
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
